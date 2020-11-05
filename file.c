5575c84ff6f913fe1adebc4123ef4f1a5882ec9c
Сёмка <baroevsmyon999@gmail.com>
2020-11-05 12:36:38 +0300

    Com4

--- file.c
+++ file.c
@@ -1,19 +1,11 @@
-<<<<<<< HEAD
 #include <stdio.h>
-=======
 #include headers.h
->>>>>>> vet1
 
 int main()
 {
     int i;
     i = 1+1;
-<<<<<<< HEAD
-    printf("%d\n", i); //Выводим результат
-    return 0;
-=======
-    printf("Result = %d\n", i);
+    printf("Result = %d\n", i); //Выводим результат
     printf("Fin");
-    return -1;
->>>>>>> vet1
+    return 0;
 }
